95a53746f868c54a458723bd5f91b537431b936a
William Spies <william@viam.com>
2022-10-28 21:52:44 -0600

    Commit results for OMPL 100-run sequences

--- scripts/execute_ompl_tests.sh
+++ scripts/execute_ompl_tests.sh
@@ -9,16 +9,16 @@ PLANEVAL_EXEC=$ROOT_DIR"build/planning_evaluator"      # Just goes right to the 
 SCENES=("scene1" "scene2" "scene3" "scene4" "scene5" "scene6" "scene7" "scene8" "scene9")
 
 # Main execution block
-echo "--------------- Starting batch testing for OMPL / RDK evaluation ---------------"
+echo "------------- Starting batch evaluation for OMPL / RDK integration -------------"
 for SCENE in ${SCENES[@]}
 do
   TITLE_OVERRIDE=()
-  for k in {1..10}
+  for k in {1..100}
   do
     TITLE_OVERRIDE+=("$SCENE"_"$k")
   done
 
-  echo "------------------ Starting 10 test iterations for [ $SCENE ] ------------------"
+  echo "----------------- Starting 100 test iterations for [ $SCENE ] ------------------"
 
   parallel -j 10 --link $PLANEVAL_EXEC ::: $SCENE ::: $TIME ::: $PLANNER ::: ${TITLE_OVERRIDE[@]}
 done
