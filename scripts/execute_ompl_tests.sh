fb616c3cc067b19b524b0f7e38a4c97b3677f9e3
William Spies <william@viam.com>
2022-11-11 14:43:43 -0700

    Updated data for OMPL RRTstar results at 1s, 5s

--- scripts/execute_ompl_tests.sh
+++ scripts/execute_ompl_tests.sh
@@ -4,9 +4,9 @@ TIME=20         # seconds
 PLANNER=0       # 0 = RRT*, 1 = Informed RRT*
 
 ROOT_DIR="<YOUR OMPL-EVALUATION DIRECTORY HERE>"       # Set this to whatever the absolute path of ompl-evaluation is
-PLANEVAL_EXEC=$ROOT_DIR"build/planning_evaluator"      # Just goes right to the compiled C++ executable in the build dir
+PLANEVAL_EXEC=$ROOT_DIR"/build/planning_evaluator"     # Just goes right to the compiled C++ executable in the build dir
 
-SCENES=("scene1" "scene2" "scene3" "scene4" "scene5" "scene6" "scene7" "scene8" "scene9")
+SCENES=("scene1" "scene2" "scene3" "scene4" "scene5" "scene6" "scene7" "scene8" "scene9" "scene10" "scene11" "scene12")
 
 # Main execution block
 echo "------------- Starting batch evaluation for OMPL / RDK integration -------------"
