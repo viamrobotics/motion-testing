e9209ebe8937ddf616301cd254885ccbc18e7a35
William Spies <william@viam.com>
2022-11-16 17:32:21 -0700

    Clean up results for rrtstar, add results for bitstar

--- scripts/execute_ompl_tests.sh
+++ scripts/execute_ompl_tests.sh
@@ -2,8 +2,9 @@
 
 TIME=20         # seconds
 PLANNER=0       # 0 = RRT*, 1 = Informed RRT*, 2 = BIT*, 3 = ABIT*
+K_MAX=100       # maximum number of tests to execute
 
-ROOT_DIR="<YOUR OMPL-EVALUATION DIRECTORY HERE>"       # Set this to whatever the absolute path of ompl-evaluation is
+ROOT_DIR="<YOUR OMPL-EVALUATION DIRECTORY HERE>"      # Set this to whatever the absolute path of ompl-evaluation is
 PLANEVAL_EXEC=$ROOT_DIR"/build/planning_evaluator"     # Just goes right to the compiled C++ executable in the build dir
 
 SCENES=("scene1" "scene2" "scene3" "scene4" "scene5" "scene6" "scene7" "scene8" "scene9" "scene10" "scene11" "scene12")
@@ -13,12 +14,12 @@ echo "------------- Starting batch evaluation for OMPL / RDK integration -------
 for SCENE in ${SCENES[@]}
 do
   TITLE_OVERRIDE=()
-  for k in {1..100}
+  for ((k=1; k<=K_MAX; k++))
   do
     TITLE_OVERRIDE+=("$SCENE"_"$k")
   done
 
-  echo "----------------- Starting 100 test iterations for [ $SCENE ] ------------------"
+  echo "--------------- Starting [ $K_MAX ] test iterations for [ $SCENE ] ----------------"
 
   parallel -j 10 --link $PLANEVAL_EXEC ::: $SCENE ::: $TIME ::: $PLANNER ::: ${TITLE_OVERRIDE[@]}
 done
