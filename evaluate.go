b7492f2656a86be1fad7fda1b3863f79067e305b
Peter LoVerso <biotinker@gmail.com>
2023-12-05 09:00:44 -0800

    Debug scenes

--- evaluate.go
+++ evaluate.go
@@ -21,25 +21,25 @@ var scene *motionplan.PlanRequest
 
 var allScenes = map[int]func() (*motionplan.PlanRequest, error){
 	// arm scenes
-	1:  scene1,
-	2:  scene2,
-	3:  scene3,
-	4:  scene4,
+	//~ 1:  scene1,
+	//~ 2:  scene2,
+	//~ 3:  scene3,
+	//~ 4:  scene4,
 	5:  scene5,
-	6:  scene6,
-	7:  scene7,
-	8:  scene8,
-	9:  scene9,
-	10: scene10,
-	11: scene11,
-	12: scene12,
+	//~ 6:  scene6,
+	//~ 7:  scene7,
+	//~ 8:  scene8,
+	//~ 9:  scene9,
+	//~ 10: scene10,
+	//~ 11: scene11,
+	//~ 12: scene12,
 	// base scenes
-	13: scene13,
-	14: scene14,
+	//~ 13: scene13,
+	//~ 14: scene14,
 	15: scene15,
-	16: scene16,
-	17: scene17,
-	18: scene18,
+	//~ 16: scene16,
+	//~ 17: scene17,
+	//~ 18: scene18,
 }
 
 // initScene takes a scene number and loads the relevant information into memory
